b98057f4980e151d7eb69703cd3ce35157ebd1f5
Zachery Bir <zbir@zacbir.net>
2017-09-08 22:19:46 -0400

    Provide argparse hooks for default coloring (light on dark or dark on light), and establish DEBUG for operation snapshotting.

--- generate.py
+++ generate.py
@@ -14,28 +14,31 @@ if __name__ == '__main__':
     parser.add_argument('-x', dest='width', type=int, help='width of the canvas')
     parser.add_argument('-y', dest='height', type=int, help='height of the canvas')
     parser.add_argument('-o', dest='output_dir', help='directory in which to save resulting image')
+    parser.add_argument('-c', dest='contrast', help='contrast theme: "light" or "dark"', default='light')
 
     args = parser.parse_args()
 
+    DEBUG = os.getenv("GEOMETER_DEBUG", False)
+
     script_name = os.path.splitext(args.geometer_script)[0]
-    dated_name = "{}_{}".format(script_name, datetime.now().strftime('%Y-%m-%d-%H:%M:%S'))
+    dated_name = "{}_{}x{}_{}".format(script_name, args.width, args.height, datetime.now().strftime('%Y-%m-%d-%H:%M:%S'))
 
     script = __import__('geometer_scripts.{}'.format(script_name), globals(), locals(), ['draw'], 0)
 
     outputDir = os.path.join(os.path.dirname(os.path.realpath(__file__)), args.output_dir)
     filename = os.path.join(outputDir, "{}".format(dated_name))
 
-    canvas = CoreGraphicsCanvas(filename, args.width, args.height)
+    canvas = CoreGraphicsCanvas(filename, args.width, args.height, debug=DEBUG)
     canvas.set_miter_limit(15)
     canvas.set_line_cap(kCGLineCapRound)
     canvas.set_line_join(kCGLineJoinMiter)
     canvas.set_stroke_color(clear)
     canvas.set_stroke_width(4)
-    canvas.set_fill_color(base03)
+    canvas.set_fill_color(base3 if args.contrast == "light" else base03)
     
     canvas.fill_background()
     
-    canvas.set_stroke_color(base1)
+    canvas.set_stroke_color(base01 if args.contrast == "light" else base1)
     canvas.set_fill_color(clear)
     
     try:
