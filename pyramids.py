9860f6ef3dfeb7e7e84288250c81a39fa45b6ce3
Zachery Bir <zbir@zacbir.net>
2017-11-18 09:55:05 -0500

    Changing the order of opacity to better reflect the illusion

--- pyramids.py
+++ pyramids.py
@@ -44,10 +44,22 @@ def random_point_within(min_x, max_x, min_y, max_y):
     return Point(rand_x, rand_y)
 
 
+def color_for_point(point, canvas):
+    """
+
+    :param point: Point
+    :param canvas: CoreGraphicsCanvas
+    :return: Color
+    """
+    line = Line.from_origin_with_slope(Point(canvas.width, 0), 2.0/5.0)
+    distance = line.distance_to(point)
+    return band(fills, distance, canvas.diagonal, fuzz=True)
+
+
 def draw(canvas):
 
-    blocks = 10
-    inner_margin = 50
+    blocks = 5
+    inner_margin = 75
 
     bounds_w = canvas.width / (blocks + 2)
     bounds_h = canvas.height / (blocks + 2)
@@ -86,21 +98,35 @@ def draw(canvas):
 
             sides = [west, north, east, south]
 
-            fill_one = range(4)                          # Either fill one triangle
-            fill_two = zip(range(4), range(1, 4) + [0])  # Or two, adjacent triangles
+            color = base00  # color_for_point(Point(min_x, min_y), canvas)
 
-            fill_chance = random.random()
-
-            if fill_chance < 0.5:
-                pair = random.choice(fill_two)
-                triangles = sides[pair[0]], sides[pair[1]]
-                with ContextStroker(canvas, 0, clear) as fill_only_canvas:
-                    with ContextFiller(fill_only_canvas, base1) as filled_canvas:
-                        triangles[0].draw(filled_canvas)
-                    with ContextFiller(fill_only_canvas, base00) as filled_canvas:
-                        triangles[1].draw(filled_canvas)
-            elif 0.5 < fill_chance < 0.65:
-                triangle = sides[random.choice(fill_one)]
-                with ContextStroker(canvas, 0, clear) as fill_only_canvas:
-                    with ContextFiller(fill_only_canvas, base1) as filled_canvas:
-                        triangle.draw(filled_canvas)
+            with ContextStroker(canvas, 0, clear) as fill_only_canvas:
+                with ContextFiller(fill_only_canvas, color.alpha(0.5)) as west_canvas:
+                    west.draw(west_canvas)
+                with ContextFiller(fill_only_canvas, color) as north_canvas:
+                    north.draw(north_canvas)
+                with ContextFiller(fill_only_canvas, color.alpha(0.75)) as east_canvas:
+                    east.draw(east_canvas)
+                with ContextFiller(fill_only_canvas, color.alpha(0.25)) as south_canvas:
+                    south.draw(south_canvas)
+            #
+            # fill_one = range(4)                          # Either fill one triangle
+            # fill_two = zip(range(4), range(1, 4) + [0])  # Or two, adjacent triangles
+            #
+            # fill_chance = random.random() * (plot_h_idx + 1)
+            #
+            # # fill_chance += Point(min_x, min_y).distance_to(origin) / canvas.diagonal
+            #
+            # if fill_chance < 1:
+            #     pair = random.choice(fill_two)
+            #     triangles = sides[pair[0]], sides[pair[1]]
+            #     with ContextStroker(canvas, 0, clear) as fill_only_canvas:
+            #         with ContextFiller(fill_only_canvas, base1) as filled_canvas:
+            #             triangles[0].draw(filled_canvas)
+            #         with ContextFiller(fill_only_canvas, base00) as filled_canvas:
+            #             triangles[1].draw(filled_canvas)
+            # elif 1 < fill_chance < 1.75:
+            #     triangle = sides[random.choice(fill_one)]
+            #     with ContextStroker(canvas, 0, clear) as fill_only_canvas:
+            #         with ContextFiller(fill_only_canvas, base1) as filled_canvas:
+            #             triangle.draw(filled_canvas)
